ca06cf024b2d36c932b67b524b0fec9c7e7ef2e7
Nicbarba22 <nicobarbadominguez@gmail.com>
2025-05-22 13:37:08 +0200

    Update usuarioController.php

--- Entorno Desarrollo PHP/public/controllers/usuarioController.php
+++ Entorno Desarrollo PHP/public/controllers/usuarioController.php
@@ -6,8 +6,6 @@ class usuarioController {
 
     /*
         Inicia sesión del usuario
-        Parámetros: no tiene
-        Retorna: no tiene, redirige a la vista correspondiente según el rol del usuario
     */
     public function iniciarSesion() {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -16,14 +14,14 @@ class usuarioController {
             $user = $userDAO->iniciarSesion($_POST['NOMBRE'], $_POST['CONTRA']);
 
             if ($user) {
-                // Iniciar sesión y almacenar el rol del usuario
+                // Iniciaremos sesión y almacenamos el rol del usuario
                 if (session_status() == PHP_SESSION_NONE) {
                     session_start();
                 }
                 $_SESSION['usuario'] = $user;
                 
             } else {
-                // Mostrar mensaje de error con SweetAlert
+                // Mostrará mensaje de error
                 $error = "Nombre o contraseña incorrectos";
                 View::show("login", ['error' => $error]);
             }
