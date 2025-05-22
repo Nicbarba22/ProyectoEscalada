7f0ca139f95df9b902c11d75aa852818a88c272d
Nicbarba22 <nicobarbadominguez@gmail.com>
2025-05-22 13:40:21 +0200

    Update View.php

--- Entorno Desarrollo PHP/public/views/View.php
+++ Entorno Desarrollo PHP/public/views/View.php
@@ -6,13 +6,13 @@
 class View {
     
     /*
-        Método estático para mostrar una vista. Construye la vista utilizando la cabecera, la vista a mostrar y el pie de página. 
-        Se le pueden pasar datos a la vista a través del parámetro $data.
-        Parámetros:
-            $viewName: nombre de la vista a mostrar.
-            $data: parámetro, de tipo array indexado, que contiene los datos que serán utilizados en la vista. Si no 
-            necesita datos $data será null.
-        Retorna: no tiene, incluye los archivos de cabecera, vista y pie de página.
+        Método estático que muestra una vista construyendo la cabecera la vista principal
+        y el pie de página se pueden pasar datos a la vista mediante el parámetro \$data 
+        Parámetros 
+        \$viewName nombre de la vista a mostrar
+        \$data array indexado con los datos para la vista puede ser null si no se necesitan datos
+        Retorna no tiene retorno incluye los archivos de cabecera vista y pie de página
+
     */
     public static function show ($viewName, $data=null){
         include_once ("header.php");
